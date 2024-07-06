c04806e111a3968ded034127309e55af202bb68d
joan <joan.aquino.vasquez@gmail.com>
2024-07-06 13:51:32 -0500

    Cambios

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,18 +3,18 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.facebook.com/JoanAquinoVasquez'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://x.com/JoanAqu22593051/'>
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/joan_aquinovasquez/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado por Joan Edinson Aquino Vasquez</strong>
     </footer>
 }
 
